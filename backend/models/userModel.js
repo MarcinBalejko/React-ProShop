ca8888394110f39724ec3913b4af62f5b52aadd5
MarcinBalejko <mbbm9@o2.pl>
2021-01-26 02:55:51 +0100

    :white_check_mark::zap: Created user authentication endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,11 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+// ^^^ creating a method that can be accessed with an instantiated user
 
 const User = mongoose.model("User", userSchema);
 
