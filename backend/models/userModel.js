1841ce129b5ed3e718b2a714c1e70b25328af66d
MarcinBalejko <mbbm9@o2.pl>
2021-01-28 19:47:46 +0100

    :white_check_mark::lock: Implemented user password encryption with bcryptjs

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,20 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 // ^^^ creating a method that can be accessed with an instantiated user
 
+userSchema.pre("save", async function (next) {
+  // ^^^ ONLY want to run this when PASSWORD changes
+  if (!this.isModified("password")) {
+    next();
+  }
+  // ^^^checking whether password field is modified
+  // if it hasn't been modified then we just run next() and move on
+  // it will automatically run 'pre' 'save' (when 'create' user)
+  // so we don't have to bring it into user controller
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
