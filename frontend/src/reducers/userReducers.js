1b599a1d69232c5cb06690d129cc9c25dc80d47b
MarcinBalejko <mbbm9@o2.pl>
2021-02-02 02:31:09 +0100

    :wrench: Navbar name bug fixed

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -70,7 +70,7 @@ export const userUpdateProfileReducer = (state = {}, action) => {
     case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
     case USER_UPDATE_PROFILE_RESET:
-      return { user: {} };
+      return {};
     default:
       return state;
   }
