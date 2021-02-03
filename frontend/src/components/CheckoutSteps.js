a031e93f75f778aa3164a7e11be0cf89fb933dc8
MarcinBalejko <mbbm9@o2.pl>
2021-02-03 20:42:32 +0100

    :recycle::pencil2: Typos corrected

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -11,37 +11,37 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
             <Nav.Link>Sign In</Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.link disabled>Sign In</Nav.link>
+          <Nav.Link disabled>Sign In</Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
-        {step1 ? (
+        {step2 ? (
           <LinkContainer to="/shipping">
             <Nav.Link>Shipping</Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.link disabled>Shipping</Nav.link>
+          <Nav.Link disabled>Shipping</Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
-        {step1 ? (
+        {step3 ? (
           <LinkContainer to="/payment">
             <Nav.Link>Payment</Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.link disabled>Payment</Nav.link>
+          <Nav.Link disabled>Payment</Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
-        {step1 ? (
+        {step4 ? (
           <LinkContainer to="/placeorder">
             <Nav.Link>Place Order</Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.link disabled>Place Order</Nav.link>
+          <Nav.Link disabled>Place Order</Nav.Link>
         )}
       </Nav.Item>
     </Nav>
