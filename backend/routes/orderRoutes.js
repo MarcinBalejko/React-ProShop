9fc7272676ba4e5fbd2f2c9c1b9a4798cc26dcdf
MarcinBalejko <mbbm9@o2.pl>
2021-02-18 23:04:51 +0100

    :wrench::bug: Minor order routes bug fixed

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,3 +4,5 @@ import { addOrderItems } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+
+export default router;
