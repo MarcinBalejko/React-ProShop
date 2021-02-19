4a717856c228f4f6041935cf08c91ad456fc2974
MarcinBalejko <mbbm9@o2.pl>
2021-02-19 23:08:30 +0100

    :white_check_mark::zap: Created an endpoint updating user's order to paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import { request } from "express";
 import asyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
 
@@ -38,7 +39,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 });
 
 //@desc Get order by ID
-// @route get /api/orders/:ID
+// @route GET /api/orders/:id
 // @access Private
 const getOrderById = asyncHandler(async (req, res) => {
   // also want to get user's name and email associated with this order
@@ -56,4 +57,30 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+//@desc Update order to paid
+// @route GET /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  // paymentResult's data will come from the paypal response
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
